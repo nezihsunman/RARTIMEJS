25d84a2a63da01c79110889e9a630b34764f66e7
Deniz Gökçin <dgokcin@gmail.com>
2019-11-26 00:16:46 +0300

    get rid of dummy job class

--- src/test/java/PriorityQueueTests.java
+++ src/test/java/PriorityQueueTests.java
@@ -1,6 +1,9 @@
 package test.java;
 
-import main.java.SingletonJobQueue.AbstractJob;
+import main.java.AbstractJobFactory.AbstractJobFactory;
+import main.java.AbstractJobFactory.FindMaxJobFactory;
+import main.java.AbstractJobFactory.SortingJobFactory;
+import main.java.Jobs.AbstractJob;
 import main.java.SingletonJobQueue.JobQueue;
 
 import org.junit.Assert;
@@ -17,17 +20,29 @@ public class PriorityQueueTests {
     }
 
     @Test
-    public void test_Should_Remove_Highest_Priority() {
+    public void test_Should_Remove_Lowest_Priority() {
         JobQueue singletonQueue = JobQueue.getInstance();
-        AbstractJob j1 = new AbstractJob(5);
-        AbstractJob j2 = new AbstractJob(8);
-        AbstractJob j3 = new AbstractJob(1);
+        int cpu1=1;
+        int ram1=16;
+        int jobTime1=10;
 
-        singletonQueue.add(j1);
-        singletonQueue.add(j2);
-        singletonQueue.add(j3);
+        int ram2=32;
+        int cpu2=2;
+        int jobTime2=20;
 
-        Assert.assertEquals(singletonQueue.remove(), j2);
+        AbstractJobFactory jf_max = new FindMaxJobFactory();
+        AbstractJobFactory jf_sort = new SortingJobFactory();
+
+        AbstractJob maxJob = jf_max.getJob(cpu1,ram1,jobTime1);
+        AbstractJob sortJob = jf_sort.getJob(cpu2,ram2,jobTime2);
+
+        maxJob.setPriority(jobTime1);
+        sortJob.setPriority(jobTime2);
+
+        singletonQueue.add(maxJob);
+        singletonQueue.add(sortJob);
+
+        Assert.assertEquals(singletonQueue.remove(), maxJob);
 
     }
 }
